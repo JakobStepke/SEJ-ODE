afebd91037119db38bc141775145e45cbd42dc67
Emil <e12209452@student.tuwien.ac.at>
2024-01-03 21:35:18 +0100

    broken

--- mass_spring/mass_spring.cc
+++ mass_spring/mass_spring.cc
@@ -15,16 +15,20 @@ int main()
 
 
   double tend = 10;
-  double steps = 1000;
+  double steps = 5;
   
   Vector<> x(2*mss.Masses().size());
   Vector<> dx(2*mss.Masses().size());  
   Vector<> ddx(2*mss.Masses().size());  
 
+  cout << "x.Size() = " << x.Size() << endl;
+
   auto mss_func = make_shared<MSS_Function<2>> (mss);
   auto mass = make_shared<IdentityFunction> (x.Size());      
 
   mss.GetState (x, dx, ddx);
+
+  cout << "x = " << x << endl;
   
   SolveODE_Newmark(tend, steps, x, dx,  mss_func, mass,
                    [](double t, VectorView<double> x) { cout << "t = " << t
